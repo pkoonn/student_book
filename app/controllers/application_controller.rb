68d079a2c4fb28ffc294b8116d6cd49fcf0b62bd
pkoonn <i13moriwkai@gmail.com>
2017-05-22 02:47:00 +0900

    ログインするための追加情報を登録できるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    allow_parameters = %i[user_name name kana number course_id type]
+    devise_parameter_sanitizer.permit(:sign_up, keys: allow_parameters)
+    devise_parameter_sanitizer.permit(:account_update, keys: allow_parameters)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
   attr_accessor :login
 
   def self.find_first_by_auth_conditions(warden_conditions)
@@ -22,4 +22,3 @@ class User < ApplicationRecord
     false
   end
 end
-Contact GitHub API Training Shop Blog About
