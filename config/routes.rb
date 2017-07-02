06103e1ef6d2b691b6204486d3289d5842c4b81f
pkoonn <i13moriwkai@gmail.com>
2017-07-03 07:00:57 +0900

    出席情報入力部分

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'dashboards#index'
+  resources :teachers, only: [] do
+    resources :subjects do
+      get '/attendances/:count', to: 'subjects/attendances#show', as: :attendance
+    end
+  end
+  resources :students, only: [] do
+    resources :subjects
+  end
+  resources :attendances
 end
