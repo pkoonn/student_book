a220a486344f1e5e760851eb18e365eb69389cf6
pkoonn <i13moriwkai@gmail.com>
2017-07-03 18:57:40 +0900

    seedデータ記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,120 +6,96 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-teacher = Teacher.create(name: 'admin', kana: 'admin', number: 'admin', user_name: 'admin', course_id: 1, password: 'hogehoge')
-student = Student.create(name: 'student', kana: 'student', number: 'student', user_name: 'student', course_id: 1, password: 'hogehoge')
+teacher = Teacher.create(name: 'takayama', kana: 'takayama', number: 'admin', user_name: 'admin', course_id: 1, password: 'hogehoge')
+student = Student.create(name: '生徒', kana: 'せいと', number: '1', user_name: 'student', course_id: 1, password: 'hogehoge')
 
 subject = teacher.subjects.create(
+  teacher_id: 1,
   timetable: [3, 4],
   name: '自然言語処理(本科5年)',
   order: [1],
   year: 5,
   semester: 0,   #0:通年(30回),1:半期(15回)    day_of_week:5,  # 1=月曜日からスタート
-  day_of_week: 5,
-# )
-# user1 = User.create(
-#     password: '000000',
-#     sign_in_count: '0',
-#     name:'高山泰博',
-#     kana:'たかやまやすひろ'
-#     number:'aaaaaaaa',
-#     username:'takayama',
-#     corse_id:'0',   # いまのところ先生の場合は0
-#     type:'0',       # 0:先生,1:生徒
-#     )
-# user2 = User.create(
-#     password: '000000',
-#     sign_in_count: '0',
-#     name:'山田健仁',
-#     kana:'やまだけんじ'
-#     number:'aaaaaaaa',
-#     username:'yamada',
-#     corse_id:'0',
-#     type:'0',       # 0:先生,1:生徒
-#     )
-# user3 = User.create(
-#     password: '000000',
-#     sign_in_count: '0',
-#     name:'石川真子',
-#     kana:'いしかわまこ'
-#     number:'4',
-#     username:'i13isikasa',
-#     corse_id:'1',
-#     type:'1',       # 0:先生,1:生徒
-#     )
-# user4 = User.create(
-#     password: '000000',
-#     sign_in_count: '0',
-#     name:'原田慎太郎',
-#     kana:'はらだしんたろう'
-#     number:'23',
-#     username:'i13harada',
-#     corse_id:'1',
-#     type:'1',       # 0:先生,1:生徒
-#     )
-# user5 = User.create(
-#     password: '000000',
-#     sign_in_count: '0',
-#     name:'廣澤勇樹',
-#     kana:'ひろざわゆうき'
-#     number:'26',
-#     username:'i13hirozawa',
-#     corse_id:'1',
-#     type:'1',       # 0:先生,1:生徒
-#     )
-# user6 = User.create(
-#     password: '000000',
-#     sign_in_count: '0',
-#     name:'森脇友香',
-#     kana:'もりわきともか'
-#     number:'33',
-#     username:'i13moriwaki',
-#     corse_id:'1',
-#     type:'1',       # 0:先生,1:生徒
-#     )
-# user7 = User.create(
-#     password: '000000',
-#     sign_in_count: '0',
-#     name:'山縣将貴',
-#     kana:'やまがたまさき'
-#     number:'35',
-#     username:'i13yamagata',
-#     corse_id:'1',
-#     type:'1',       # 0:先生,1:生徒
-#     )
-#   subject1 = Subject.create(
-#     user_id:user1.id,
-#     timetables:[3,4],
-#     name:'自然言語処理(本科5年)',
-#     order:
-#     year:5
-#     semester:0,   #0:通年(30回),1:半期(15回)
-#     day_of_week:5,  # 1=月曜日からスタート
-#     )
-# user_subject = UserSubject.create({
-#     user_id:user1.id,
-#     subject_id:1
-#   },
-#   {
-#     user_id:user3.id,
-#     subject_id:1
-#   },
-#   {
-#     user_id:user4.id,
-#     subject_id:1
-#   },
-#   {
-#     user_id:user5.id,
-#     subject_id:1
-#   },
-#   {
-#     user_id:user6.id,
-#     subject_id:1
-#   },
-#   {
-#     user_id:user7.id,
-#     subject_id:1
-#   })
+  day_of_week: 5
+)
+teacher1 = Teacher.create(
+    password: '000000',
+    sign_in_count: '0',
+    name:'山田健仁',
+    kana:'やまだけんじ',
+    number:'aaaaaaaa',
+    user_name:'yamada',
+    course_id:'1'
+    )
+student1 = Student.create(
+    password: '000000',
+    sign_in_count: '0',
+    name:'石川真子',
+    kana:'いしかわまこ',
+    number:'4',
+    user_name:'i13isikasa',
+    course_id:'1'
+    )
+student2 = Student.create(
+    password: '000000',
+    sign_in_count: '0',
+    name:'原田慎太郎',
+    kana:'はらだしんたろう',
+    number:'23',
+    user_name:'i13harada',
+    course_id:'1'
+    )
+student3 = Student.create(
+    password: '000000',
+    sign_in_count: '0',
+    name:'廣澤勇樹',
+    kana:'ひろざわゆうき',
+    number:'26',
+    user_name:'i13hirozawa',
+    course_id:'1'
+    )
+student4 = Student.create(
+    password: '000000',
+    sign_in_count: '0',
+    name:'森脇友香',
+    kana:'もりわきともか',
+    number:'33',
+    user_name:'i13moriwaki',
+    course_id:'1'
+    )
+student5 = Student.create(
+    password: '000000',
+    sign_in_count: '0',
+    name:'山縣将貴',
+    kana:'やまがたまさき',
+    number:'35',
+    user_name:'i13yamagata',
+    course_id:'1'
+    )
+  student_subject = StudentSubject.create(
+    student_id:student1.id,
+    subject_id:1
+  )
+  student_subject = StudentSubject.create(
+    student_id:student2.id,
+    subject_id:1
+  )
+  student_subject = StudentSubject.create(
+    student_id:student3.id,
+    subject_id:1
+  )
+  student_subject = StudentSubject.create(
+    student_id:student4.id,
+    subject_id:1
+  )
+  student_subject = StudentSubject.create(
+    student_id:student5.id,
+    subject_id:1
+  )
+  attendances = Attendance.create(
+    student_id: student1.id,
+    subject_id: 1
+  )
 
 subject.student_subjects.create
 
